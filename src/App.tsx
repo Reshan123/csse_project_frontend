6a8ae11ae12b2c6f45844f543b0f326fcf5eae7c
Hirusha Jayarathne <hirushachamod2@gmail.com>
2024-10-13 18:54:30 +0530

    Added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Route, Routes } from "react-router-dom"
+
+import HomePage from "./views/patient/HomePage"
 
 function App() {
  
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Routes>
+      <Route path="/" element={<HomePage/>}/>
+    </Routes>
   )
 }
 
