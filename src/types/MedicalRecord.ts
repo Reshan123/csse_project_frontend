3441341603ca7a0c98018de13cb66cb084aba5f1
Hirusha Jayarathne <hirushachamod2@gmail.com>
2024-10-13 10:23:45 +0530

    Patient id generation

--- src/types/MedicalRecord.ts
+++ src/types/MedicalRecord.ts
@@ -1,15 +1,14 @@
 export interface MedicalRecord {
-  userId: string | null;
-  id?: string;
-  patientId: string;
-  firstName: string;
-  lastName: string;
-  dateOfBirth: string;
-  gender: string;
-  contactNumber: string;
-  address: string;
-  allergies: string[];
-  ongoingMedications: string[];
-  emergencyContactName: string;
-  emergencyContactNumber: string;
-}
+    id?: string;
+    patientId: string;
+    firstName: string;
+    lastName: string;
+    dateOfBirth: string;
+    gender: string;
+    contactNumber: string;
+    address: string;
+    allergies: string[];
+    ongoingMedications: string[];
+    emergencyContactName: string;
+    emergencyContactNumber: string;
+  }
